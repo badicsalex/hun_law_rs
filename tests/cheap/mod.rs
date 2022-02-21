1d76767d354b5a37f3fb84cfd065288131d00092
Alex Badics <admin@stickman.hu>
2022-02-21 21:29:49 +0100

    project: added copyright header to alltest files

--- tests/cheap/mod.rs
+++ tests/cheap/mod.rs
@@ -1,4 +1,19 @@
+// Copyright (C) 2022, Alex Badics
+//
+// This file is part of Hun-Law.
+//
+// Hun-law is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3 of the License.
+//
+// Hun-law is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Hun-law. If not, see <http://www.gnu.org/licenses/>.
+
 mod test_cache;
 
 pub mod util;
-
--- tests/cheap/test_cache.rs
+++ tests/cheap/test_cache.rs
@@ -1,3 +1,19 @@
+// Copyright (C) 2022, Alex Badics
+//
+// This file is part of Hun-Law.
+//
+// Hun-law is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3 of the License.
+//
+// Hun-law is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Hun-law. If not, see <http://www.gnu.org/licenses/>.
+
 use super::util::{cache_in_tempdir, CacheInTempDir};
 use anyhow::Result;
 use rstest::rstest;
