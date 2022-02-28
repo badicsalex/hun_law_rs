9675d6b0d2eef2b424d1870afeae7ca853f6d916
Alex Badics <admin@stickman.hu>
2022-02-28 19:58:07 +0100

    pdf_parser: refactor, add bold font detection

--- src/pdf_parser.rs
+++ src/pdf_parser.rs
@@ -27,9 +27,9 @@ const SPACE_DETECTION_THRESHOLD_RATIO: f64 = 0.5;
 struct PositionedChar {
     x: f64,
     y: f64,
-    #[allow(dead_code)]
     width: f64,
     width_of_space: f64,
+    bold: bool,
     first_char_of_word: bool,
     content: char,
 }
@@ -39,23 +39,90 @@ struct PageOfPositionedChars {
     chars: Vec<PositionedChar>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct PdfExtractor {
     pages: Vec<PageOfPositionedChars>,
     width_of_space: f64,
+    current_font_is_bold: bool,
     first_char_incoming: bool,
 }
 
-fn fix_character_coding_quirks(c: char) -> char {
-    match c {
-        'Õ' => 'Ő', // Note the ~ on top of the first ő
-        'õ' => 'ő', // Note the ~ on top of the first ő
-        'Û' => 'Ű', // Note the ^ on top of the first ű
-        'û' => 'ű', // Note the ^ on top of the first ű
-        '\u{a0}' => ' ',
-        _ => c,
+#[derive(Debug)]
+pub struct PageOfLines {
+    pub lines: Vec<String>,
+}
+
+impl PdfExtractor {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    fn consolidate_line(mut chars: Vec<PositionedChar>) -> String {
+        if chars.is_empty() {
+            return String::new();
+        }
+        chars.sort_unstable_by(|c1, c2| compare_float_for_sorting(c1.x, c2.x));
+        let mut result: String = String::new();
+        let mut threshold_to_space = f64::INFINITY;
+        let mut last_was_space = false;
+        for current_char in &chars {
+            if current_char.x > threshold_to_space
+                && current_char.first_char_of_word
+                && !last_was_space
+                && current_char.content != ' '
+            {
+                result.push(' ');
+            }
+            result.push(current_char.content);
+            threshold_to_space = current_char.x
+                + current_char.width
+                + current_char.width_of_space * SPACE_DETECTION_THRESHOLD_RATIO;
+            last_was_space = current_char.content == ' ';
+        }
+        if chars.iter().all(|c| c.bold) {
+            result = format!("* {} *", result)
+        }
+        let indent = (chars[0].x * 0.1) as usize;
+        result.insert_str(0, &" ".repeat(indent));
+        result
+    }
+
+    fn consolidate_page(page: PageOfPositionedChars) -> Result<PageOfLines> {
+        let mut result = Vec::<String>::new();
+        let mut chars = page.chars;
+        chars.sort_unstable_by(|c1, c2| compare_float_for_sorting(c2.y, c1.y));
+        let mut current_line = Vec::<PositionedChar>::new();
+        for current_char in chars {
+            let y_diff = if current_line.is_empty() {
+                0.0
+            } else {
+                (current_line[0].y - current_char.y).abs()
+            };
+            // TODO: instad of 0.2, use some real line height thing
+            // 0.2 is small enough not to trigger for the e.g. the 2 in "m2" (the unit).
+            // And this is okay for now
+            if y_diff < SAME_LINE_EPSILON {
+                current_line.push(current_char);
+            } else {
+                result.push(Self::consolidate_line(current_line));
+                // Add empty line on a "big-enough gap"
+                // Should be based on actual font height, but this is
+                // good enough for the rest of the parsing steps.
+                if y_diff > ADDITIONAL_EMPTY_LINE_THRESHOLD {
+                    result.push("".to_string());
+                }
+                current_line = vec![current_char];
+            }
+        }
+        result.push(Self::consolidate_line(current_line));
+        Ok(PageOfLines { lines: result })
+    }
+
+    pub fn get_parsed_pages(self) -> Result<Vec<PageOfLines>> {
+        self.pages.into_iter().map(Self::consolidate_page).collect()
     }
 }
+
 impl pdf_extract::OutputDev for PdfExtractor {
     fn begin_page(
         &mut self,
@@ -100,6 +167,7 @@ impl pdf_extract::OutputDev for PdfExtractor {
                     width,
                     width_of_space: self.width_of_space * trm.m11 * font_size,
                     content: fix_character_coding_quirks(raw_char),
+                    bold: self.current_font_is_bold,
                     first_char_of_word: self.first_char_incoming,
                 });
         self.pages
@@ -120,7 +188,9 @@ impl pdf_extract::OutputDev for PdfExtractor {
             warn!("Had to use default space width for font");
             self.width_of_space = DEFAULT_WIDTH_OF_SPACE;
         };
+        let base_font = font.get_basefont();
         self.first_char_incoming = true;
+        self.current_font_is_bold = base_font.contains("bold") || base_font.contains("Bold");
         Ok(())
     }
 
@@ -133,8 +203,15 @@ impl pdf_extract::OutputDev for PdfExtractor {
     }
 }
 
-pub struct PageOfLines {
-    pub lines: Vec<String>,
+fn fix_character_coding_quirks(c: char) -> char {
+    match c {
+        'Õ' => 'Ő', // Note the ~ on top of the first ő
+        'õ' => 'ő', // Note the ~ on top of the first ő
+        'Û' => 'Ű', // Note the ^ on top of the first ű
+        'û' => 'ű', // Note the ^ on top of the first ű
+        '\u{a0}' => ' ',
+        _ => c,
+    }
 }
 
 /// Compare floats but only for somewhat correct sorting.
@@ -145,76 +222,6 @@ fn compare_float_for_sorting(f1: f64, f2: f64) -> std::cmp::Ordering {
         std::cmp::Ordering::Less
     } else {
         std::cmp::Ordering::Greater
-    }
-}
-
-impl PdfExtractor {
-    pub fn new() -> Self {
-        PdfExtractor {
-            pages: Vec::new(),
-            width_of_space: DEFAULT_WIDTH_OF_SPACE,
-            first_char_incoming: false,
-        }
-    }
-
-    fn consolidate_line(mut chars: Vec<PositionedChar>) -> String {
-        if chars.is_empty() {
-            return String::new();
-        }
-        chars.sort_unstable_by(|c1, c2| compare_float_for_sorting(c1.x, c2.x));
-        let mut result: String = String::new();
-        let mut threshold_to_space = f64::INFINITY;
-        let mut last_was_space = false;
-        for current_char in chars {
-            if current_char.x > threshold_to_space
-                && current_char.first_char_of_word
-                && !last_was_space
-                && current_char.content != ' '
-            {
-                result.push(' ');
-            }
-            result.push(current_char.content);
-            threshold_to_space = current_char.x
-                + current_char.width
-                + current_char.width_of_space * SPACE_DETECTION_THRESHOLD_RATIO;
-            last_was_space = current_char.content == ' ';
-        }
-        result
-    }
-
-    fn consolidate_page(page: PageOfPositionedChars) -> Result<PageOfLines> {
-        let mut result = Vec::<String>::new();
-        let mut chars = page.chars;
-        chars.sort_unstable_by(|c1, c2| compare_float_for_sorting(c2.y, c1.y));
-        let mut current_line = Vec::<PositionedChar>::new();
-        for current_char in chars {
-            let y_diff = if current_line.is_empty() {
-                0.0
-            } else {
-                (current_line[0].y - current_char.y).abs()
-            };
-            // TODO: instad of 0.2, use some real line height thing
-            // 0.2 is small enough not to trigger for the e.g. the 2 in "m2" (the unit).
-            // And this is okay for now
-            if y_diff < SAME_LINE_EPSILON {
-                current_line.push(current_char);
-            } else {
-                result.push(Self::consolidate_line(current_line));
-                // Add empty line on a "big-enough gap"
-                // Should be based on actual font height, but this is
-                // good enough for the rest of the parsing steps.
-                if y_diff > ADDITIONAL_EMPTY_LINE_THRESHOLD {
-                    result.push("".to_string());
-                }
-                current_line = vec![current_char];
-            }
-        }
-        result.push(Self::consolidate_line(current_line));
-        Ok(PageOfLines { lines: result })
-    }
-
-    pub fn get_parsed_pages(self) -> Result<Vec<PageOfLines>> {
-        self.pages.into_iter().map(Self::consolidate_page).collect()
     }
 }
 
