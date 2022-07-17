52cd5430de75531e5849a3f2554dc4a14740d841
Alex Badics <admin@stickman.hu>
2022-07-17 23:44:46 +0200

    tests/semantic: extend test format, test abbreviations too

--- tests/datatests/test_semantic_parser.rs
+++ tests/datatests/test_semantic_parser.rs
@@ -17,44 +17,75 @@
 use hun_law::{
     parser::semantic_info::{abbreviation::AbbreviationCache, extract_semantic_info},
     reference::Reference,
-    structure::ActIdentifier,
+    structure::{
+        semantic_info::{ActIdAbbreviation, OutgoingReference},
+        ActIdentifier,
+    },
 };
 
+use datatest_stable::Result;
 use serde::Deserialize;
 use std::{collections::HashMap, path::Path};
 
 use crate::test_utils::read_all;
 
-use pretty_assertions::assert_eq;
-
 #[derive(Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
 struct TestCase {
     pub text: String,
     pub positions: String,
     #[serde(default)]
     pub abbreviations: HashMap<String, ActIdentifier>,
-    pub expected: Vec<Reference>,
+    #[serde(default)]
+    pub expected_new_abbreviations: HashMap<String, ActIdentifier>,
+    #[serde(default)]
+    pub expected_references: Vec<Reference>,
 }
 
 use crate::declare_test;
 declare_test!(dir = "data_semantic_parser", pattern = r"\.yml");
-pub fn run_test(path: &Path) -> datatest_stable::Result<()> {
+pub fn run_test(path: &Path) -> Result<()> {
     let test_case: TestCase = serde_yaml::from_slice(&read_all(path)?)?;
     let mut abbreviation_cache = AbbreviationCache::from(test_case.abbreviations);
     let semantic_info = extract_semantic_info(&test_case.text, &mut abbreviation_cache)?;
 
-    let mut parsed_refs = Vec::new();
-    let mut parsed_positions = vec![b' '; test_case.positions.len()];
+    check_references(
+        &semantic_info.outgoing_references,
+        &test_case.expected_references,
+        &test_case.text,
+        &test_case.positions,
+    )?;
+    check_abbreviations(
+        &semantic_info.new_abbreviations,
+        &test_case.expected_new_abbreviations,
+    )?;
+    Ok(())
+}
 
-    for outgoing_reference in semantic_info.outgoing_references {
+macro_rules! ensure_eq {
+    ($left: expr, $right: expr, $message: expr) => {
+        if ($left) != ($right) {
+            return Err(format!("{}\n {:#?}\n!=\n{:#?}", $message, $left, $right).into());
+        };
+    };
+}
+
+fn check_references(
+    outgoing_references: &[OutgoingReference],
+    expected: &[Reference],
+    text: &str,
+    expected_positions: &str,
+) -> Result<()> {
+    let mut parsed_refs = Vec::new();
+    let mut parsed_positions = vec![b' '; expected_positions.len()];
+
+    for outgoing_reference in outgoing_references {
         parsed_refs.push(outgoing_reference.reference.clone());
-        let start_char_index = test_case
-            .text
+        let start_char_index = text
             .char_indices()
             .position(|(cp, _)| cp == outgoing_reference.start)
             .unwrap();
-        let end_char_index = test_case
-            .text
+        let end_char_index = text
             .char_indices()
             .position(|(cp, _)| cp == outgoing_reference.end)
             .unwrap();
@@ -62,7 +93,31 @@ pub fn run_test(path: &Path) -> datatest_stable::Result<()> {
         parsed_positions[end_char_index - 1] = b'>';
     }
 
-    assert_eq!(parsed_refs, test_case.expected);
-    assert_eq!(String::from_utf8(parsed_positions)?, test_case.positions);
+    let parsed_positions = String::from_utf8(parsed_positions).unwrap();
+    ensure_eq!(&parsed_refs, expected, "References were not the same");
+    ensure_eq!(
+        &parsed_positions,
+        &expected_positions,
+        "Reference positions were not the same"
+    );
+    Ok(())
+}
+
+fn check_abbreviations(
+    new_abbreviations: &[ActIdAbbreviation],
+    expected_abbreviations: &HashMap<String, ActIdentifier>,
+) -> Result<()> {
+    let expected_abbreviations: Vec<_> = expected_abbreviations
+        .iter()
+        .map(|(k, v)| ActIdAbbreviation {
+            act_id: *v,
+            abbreviation: k.clone(),
+        })
+        .collect();
+    ensure_eq!(
+        new_abbreviations,
+        &expected_abbreviations,
+        "Abbreviations were not the same"
+    );
     Ok(())
 }
